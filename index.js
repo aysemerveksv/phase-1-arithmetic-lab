9f7aa41ba809ffdc5d753fd2c56732a4c6b3e8a8
Ayşe Merve Kosova <mervekosova1@hotmail.com>
2023-04-13 23:57:35 +0300

    completed

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 
 // Write your code here
+// Defined variables
+let num1 = 2;
+let num2 = 31;
+let num3 = 20;
+let num4 = 8;
+
+// Calculated multiplication
+let multiply = num1 * num2;
+
+// Generated random integer greater than 0
+let randomNum = Math.floor(Math.random() * 100) + 1;
+
+// Calculated remainder
+let mod = num3 % num4;
+
+// Finded maximum number
+let maxNum = Math.max(num1, num2, num3, num4);
+
+// Printed results
+console.log("Multiply: ", multiply);
+console.log("Random: ", randomNum);
+console.log("Mod: ", mod);
+console.log("Max: ", maxNum);
